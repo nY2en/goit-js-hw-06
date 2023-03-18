afc6689beb82cf4b55cdb22ae7cec6896a720b9d
Nikita Chebanenko <chebanikita@icloud.com>
2023-03-18 20:01:51 +0200

    зробив дев'яте завдання

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,9 +1,9 @@
 const refs = {
-    input: document.querySelector("#font-size-control"),
-    text: document.querySelector("#text"),
-}
+    input: document.querySelector('#font-size-control'),
+    text: document.querySelector('#text'),
+};
 
-refs.input.addEventListener("input", onInputChange);
+refs.input.addEventListener('input', onInputChange);
 
 function onInputChange(event) {
     refs.text.style.fontSize = `${event.currentTarget.value}px`;
