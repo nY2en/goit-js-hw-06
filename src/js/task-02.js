875ae224491931525b23f66d5725b5d588223868
Nikita Chebanenko <chebanikita@icloud.com>
2023-03-18 15:33:06 +0200

    зробив друге завдання

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,23 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+    'Potatoes',
+    'Mushrooms',
+    'Garlic',
+    'Tomatos',
+    'Herbs',
+    'Condiments',
 ];
+
+const ulIngredientsRef = document.querySelector('#ingredients');
+
+const makeList = elements => {
+    return elements.map(el => {
+        const item = document.createElement('li');
+        item.textContent = el;
+        item.classList.add('item');
+        return item;
+    });
+};
+
+const elements = makeList(ingredients);
+
+ulIngredientsRef.append(...elements);
