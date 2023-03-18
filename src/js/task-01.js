2ad31921eea8c656b5863ec42ee7bc5084f55081
Nikita Chebanenko <chebanikita@icloud.com>
2023-03-18 14:18:45 +0200

    зробив перше завдання

---
+++ src/js/task-01.js
@@ -0,0 +1,13 @@
+const categoriesRef = document.querySelector('#categories');
+
+const numberOfCategories = categoriesRef.children.length;
+
+console.log(`Number of categories: ${numberOfCategories}`);
+
+const categoriesTypes = [...categoriesRef.children].forEach(type => {
+    const categoryType = type.firstElementChild.textContent;
+    const numberOfElements =
+        type.firstElementChild.nextElementSibling.children.length;
+
+    console.log(`\nCategory: ${categoryType} \nElements: ${numberOfElements}`);
+});
