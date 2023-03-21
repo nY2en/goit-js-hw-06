8ee820957b81b1bba6a640c657a191d62751ccac
Nikita Chebanenko <chebanikita@icloud.com>
2023-03-21 21:20:10 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -6,7 +6,7 @@ refs.input.addEventListener('blur', onInputChange);
 
 function onInputChange(event) {
     if (
-        event.currentTarget.value.length === Number(refs.input.dataset.length)
+        event.currentTarget.value.length === Number(event.currentTarget.dataset.length)
     ) {
         refs.input.classList.remove('invalid');
         refs.input.classList.add('valid');
